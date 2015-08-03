3de81ff80d954910fb68209bd31ba90adf43a503
Joe Masilotti <joseph.masilotti@gmail.com>
2015-08-03 06:56:11 -0400

    Initial Commit

---
+++ NefariousParakeet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NefariousParakeet
+//
+//  Created by Joe Masilotti on 8/3/15.
+//  Copyright (c) 2015 BeerMenus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
